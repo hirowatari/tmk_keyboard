6c18fc19feb863682351f34848ad89b5b6d3ff53
Kenton Hirowatari <kenton@hirowatari.com>
2016-10-06 08:02:05 -0700

    Added basic programming layer

--- keyboard/hhkb/keymap_hhkb.c
+++ keyboard/hhkb/keymap_hhkb.c
@@ -11,28 +11,29 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
     /* Layer 0: Default Layer
      * ,-----------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|   |   |   |   |
      * |-----------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|   |   |     |
      * |-----------------------------------------------------------|
-     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
+     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|        |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |   |
      * `-----------------------------------------------------------'
-     *       |Alt|Gui  |         Space         |Gui  |Alt|
+     *       |Alt|Fn0  |         Space         |Gui  |   |
      *       `-------------------------------------------'
      */
-    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,   \
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
-           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
-                LALT,LGUI,          SPC,                RGUI,RALT),
+    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   TRNS,TRNS, TRNS,TRNS, \
+           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   TRNS,TRNS,TRNS,       \
+           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,TRNS,            \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,TRNS,            \
+                LALT,FN0,          SPC,                RGUI,TRNS),
 
     /* Layer 1: HHKB mode (HHKB Fn)
+     * this layer is not up to date
      * ,-----------------------------------------------------------.
-     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
+     * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
-     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |Backs|
+     * |     |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |Backs|
      * |-----------------------------------------------------------|
      * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
      * |-----------------------------------------------------------|
@@ -41,11 +42,11 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
-    KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
-           CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC,      \
-           TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
-                TRNS,TRNS,          TRNS,               TRNS,TRNS),
+    KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS, TRNS, \
+           TRNS,GRV,MINS,FN5,FN6,FN7,ESC,HOME,UP,END,PGUP, TRNS, TRNS, TRNS,      \
+           TRNS,EQL,FN8,FN9,FN10,PAST,ENT,LEFT,DOWN,RIGHT,PGDN,TRNS,TRNS,            \
+           TRNS,TRNS,TRNS,LBRC,RBRC,SLSH,BSLS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,            \
+                TRNS,TRNS,          BSPC,               TRNS,TRNS),
 };
 
 
@@ -91,5 +92,15 @@ const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
 #else
 const action_t fn_actions[] PROGMEM = {
     [0]  = ACTION_LAYER_MOMENTARY(1),
+    [1]  = ACTION_LAYER_MOMENTARY(2), // unused
+    [2]  = ACTION_LAYER_MOMENTARY(3), // unused
+    [3]  = ACTION_LAYER_MOMENTARY(4), // unused
+    [4]  = ACTION_LAYER_MOMENTARY(5), // unused
+    [5]  = ACTION_MODS_KEY(MOD_LSFT, KC_LBRACKET), // {
+    [6]  = ACTION_MODS_KEY(MOD_LSFT, KC_RBRACKET), // }
+    [7]  = ACTION_MODS_KEY(MOD_LSFT, KC_EQUAL),
+    [8]  = ACTION_MODS_KEY(MOD_LSFT, KC_MINUS), // underscore
+    [9]  = ACTION_MODS_KEY(MOD_LSFT, KC_9), // (
+    [10] = ACTION_MODS_KEY(MOD_LSFT, KC_0), // )
 };
 #endif
