1e41e94f654d88176a034d66ea5e8f7016cd4393
Kenton Hirowatari <kenton@hirowatari.com>
2017-05-11 22:09:24 -0700

    first try at keymap

--- keyboard/hhkb/keymap_jp.c
+++ keyboard/hhkb/keymap_jp.c
@@ -10,35 +10,29 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] __attribute__ ((section (".key
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 #endif
     /* Layer 0: Default Layer */
-    KEYMAP_JP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, JYEN,BSPC, \
-              TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, \
-              LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,BSLS,ENT, \
-              LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RO,  UP,  RSFT, \
-              FN0, ZKHK,LGUI,LALT,MHEN,     SPC,      HENK,KANA,RALT,FN0, LEFT,DOWN,RGHT),
+    KEYMAP_JP(GRV ,1   ,2   ,3   ,4   ,5   ,6   ,7   ,8   ,9   ,0   ,MINS,EQL ,BSPC,BSPC, \
+              TAB   ,Q   ,W   ,E   ,R   ,T   ,Y   ,U   ,I   ,O   ,P   ,LBRC,RBRC,/*ent*/  \
+              LCTL   ,A   ,S   ,D   ,F   ,G   ,H   ,J   ,K   ,L   ,SCLN,QUOT,ENT ,ENT  ,  \
+              LSFT    ,Z   ,X   ,C   ,V   ,B   ,N   ,M   ,COMM,DOT ,SLSH,RSFT,UP  ,RSFT,  \
+              LCTL ,FN1 ,NO  ,LALT,LGUI,SPC       ,FN0 ,BSPC,MENU,NO   , LEFT,DOWN,RGHT),
 
-    /* Layer 1: HHKB mode (HHKB Fn)
-     * ,-----------------------------------------------------------.
-     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
-     * |-----------------------------------------------------------|
-     * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |     |
-     * |------------------------------------------------------`    |
-     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|   |    |
-     * |-----------------------------------------------------------|
-     * |       |   |   |   |   |   |  +|  -|End|PgD|Dow|   |   |   |
-     * |-----------------------------------------------------------|
-     * |   ||   |   |   |   |         |   |   |   |   ||   |   |   |
-     * `-----------------------------------------------------------'
-     */ 
-    KEYMAP_JP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL, \
-              CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, \
-              TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,TRNS,PENT, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,TRNS, \
-              TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+    /* Layer 1: Arrows, 6 cluster (){}[]  */
+    KEYMAP_JP(ESC ,F1  ,F2  ,F3  ,F4  ,F5  ,F6  ,F7  ,F8  ,F9  ,F10 ,F11 ,F12 ,TRNS,TRNS, \
+              CAPS  ,TRNS,TRNS,TRNS,FN20,FN21,TRNS,HOME,UP  ,END ,TRNS,TRNS,TRNS,/*ent*/  \
+              TRNS   ,TRNS,TRNS,ESC ,LBRC,RBRC,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,TRNS ,  \
+              TRNS    ,BSLS,TRNS,TRNS,FN22,FN23,PGDN,DEL ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
+              TRNS ,TRNS,TRNS,TRNS,TRNS,ENT       ,TRNS,TRNS,TRNS,TRNS , TRNS,TRNS,TRNS),
+
+    /* Layer 2: media  */
+    KEYMAP_JP(PWR ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+              TRNS  ,TRNS,TRNS,TRNS,VOLU,TRNS,TRNS,HOME,PSCR,SLCK,PAUS,TRNS,TRNS,/*ent*/  \
+              TRNS   ,TRNS,TRNS,TRNS,VOLD,TRNS,TRNS,MPLY,MSTP,MPRV,MNXT,TRNS,TRNS,TRNS ,  \
+              TRNS    ,TRNS,TRNS,TRNS,MUTE,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
+              TRNS ,TRNS,TRNS,TRNS,TRNS,TRNS      ,TRNS,TRNS,TRNS,TRNS , TRNS,TRNS,TRNS)
+
 };
 
-
-
-/*
+/* next vol up and down don't seem to work right
  * Fn action definition
  */
 #ifdef KEYMAP_SECTION_ENABLE
@@ -47,4 +41,10 @@ const action_t fn_actions[] __attribute__ ((section (".keymap.fn_actions"))) = {
 const action_t fn_actions[] PROGMEM = {
 #endif
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_LAYER_MOMENTARY(2),
+    [20]  = ACTION_MODS_KEY(MOD_LSFT, KC_9),        // (
+    [21]  = ACTION_MODS_KEY(MOD_LSFT, KC_0),        // )
+    [22]  = ACTION_MODS_KEY(MOD_LSFT, KC_LBRACKET), // {
+    [23]  = ACTION_MODS_KEY(MOD_LSFT, KC_RBRACKET), // }
+
 };
